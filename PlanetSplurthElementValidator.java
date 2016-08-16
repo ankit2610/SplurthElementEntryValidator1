d885394d57d9b7dedd9c795eff3ca4b7899f71cb
ankit2610 <ankitujj@gmail.com>
2016-08-16 09:49:10 +0530

    added getFirstAlphabeticalSymbol method

--- PlanetSplurthElementValidator.java
+++ PlanetSplurthElementValidator.java
@@ -1,3 +1,5 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +17,7 @@ public class PlanetSplurthElementValidator {
 		System.out.println(obj.isValidElementEntry("Melintzum", "Nn"));
 		System.out.println(obj.isValidElementEntry("Tullium", "Ty"));
 		System.out.println("************************End Sample Outputs***********************************");
+		System.out.println(obj.getFirstAlphabeticalSymbol("Slimyrin"));
 
 		if (args.length == 2) {
 			System.out.println("************************Output for Given Input***********************************");
@@ -23,12 +26,16 @@ public class PlanetSplurthElementValidator {
 
 	}
 
+	/*
+	 * this method validates whether the entered symbol follows the rule with
+	 * the element name
+	 */
 	public boolean isValidElementEntry(String element, String symbol) {
 		boolean result = false;
 		if (element.trim().length() == 0 || symbol.trim().length() == 0) {
 			System.out.println("Element name or Symbol name can not be blank!! please enter values");
 			result = false;
-		} else if (!Pattern.matches("^[a-zA-Z]", element) || !Pattern.matches("^[a-zA-Z]", symbol)) {
+		} else if (!Pattern.matches("[A-Za-z]+", element) || !Pattern.matches("[A-Za-z]+", symbol)) {
 			System.out.println("Element name or Symbol name can contains only letters (a to z and A-Z) ");
 			result = false;
 
@@ -50,7 +57,27 @@ public class PlanetSplurthElementValidator {
 			}
 		}
 		return result;
+	}
 
+	/*
+	 * this method return a Symbol with first Alphabetical order
+	 */
+
+	String getFirstAlphabeticalSymbol(String element) {
+		element = element.toLowerCase();
+		char firstChar = element.charAt(0);
+		for (int i = 1; i < element.length() - 1; i++) {
+			if (element.charAt(i) < firstChar)
+				firstChar = element.charAt(i);
+		}
+		char secondChar = element.charAt(element.indexOf(firstChar) + 1);
+
+		for (int i = element.indexOf(firstChar) + 1; i < element.length(); i++) {
+			if (element.charAt(i) < secondChar)
+				secondChar = element.charAt(i);
+		}
+
+		return String.valueOf(firstChar).toUpperCase() + String.valueOf(secondChar);
 	}
 
 }
